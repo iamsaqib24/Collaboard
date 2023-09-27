8d167635b6465fc966bf21fc0e6568ec54b0d3bc
Saqib <iamsaqib24@gmail.com>
2023-09-28 01:32:38 +0530

    created slice for toolbox

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,11 @@
-const { useRef, useEffect } = require("react");
+import { useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Board = () => {
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const canvasRef = useRef(null);
 
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
   useEffect(() => {
     if (!canvasRef.current) return;
 
