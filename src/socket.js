7612415f2e2010938f568662f92e28f49d141691
Saqib <iamsaqib24@gmail.com>
2023-10-01 10:54:15 +0530

    added backend endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000/");
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://collaboard-server.onrender.com"
+    : "http://localhost:8000/";
+
+export const socket = io(URL);
